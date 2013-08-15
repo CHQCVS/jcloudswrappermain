1ab5c75ee206c15d81ce769d58b66992eaedc966
mroowa000 <mroowa000@ab7ae17a-21d2-4d85-b253-9b7255c381ee>
2013-08-15 15:40:44 +0000

    Using a Logger
    git-svn-id: http://svn2.cvs.ula.comcast.net/svn/cvs-xre-apps/trunk/common/jcloudswrappermain@11019 ab7ae17a-21d2-4d85-b253-9b7255c381ee

--- jcloudswrapper-maven-test/src/test/java/com/comcast/tvx/xreapps/common/deployment/MavenPropertiesIT.java
+++ jcloudswrapper-maven-test/src/test/java/com/comcast/tvx/xreapps/common/deployment/MavenPropertiesIT.java
@@ -1,19 +1,24 @@
 package com.comcast.tvx.xreapps.common.deployment;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MavenPropertiesIT {
 	
+	private static final Logger LOG = Logger.getLogger("MavenPropertiesIT");
+	
 	@Test
 	public void testMavenProperties() {
-		System.out.println("Hostname is: " + System.getProperty("cmc.os.hostname.1") );
-		System.out.println("Private IP is: " + System.getProperty("cmc.os.privateIP.1"));
-		System.out.println("Public IP is: " + System.getProperty("cmc.os.publicIP.1"));
-		System.out.println("***********************************************************");
-		System.out.println("Hostname is: " + System.getProperty("cmc.os.hostname.2") );
-		System.out.println("Private IP is: " + System.getProperty("cmc.os.privateIP.2"));
-		System.out.println("Public IP is: " + System.getProperty("cmc.os.publicIP.2"));		
+		LOG.log(Level.INFO, "Hostname is: " + System.getProperty("cmc.os.hostname.1") );
+		LOG.log(Level.INFO, "Private IP is: " + System.getProperty("cmc.os.privateIP.1"));
+		LOG.log(Level.INFO, "Public IP is: " + System.getProperty("cmc.os.publicIP.1"));
+		LOG.log(Level.INFO, "***********************************************************");
+		LOG.log(Level.INFO, "Hostname is: " + System.getProperty("cmc.os.hostname.2") );
+		LOG.log(Level.INFO, "Private IP is: " + System.getProperty("cmc.os.privateIP.2"));
+		LOG.log(Level.INFO, "Public IP is: " + System.getProperty("cmc.os.publicIP.2"));		
 		
 		Assert.assertNotNull(System.getProperty("cmc.os.hostname.1"));
 		Assert.assertNotNull(System.getProperty("cmc.os.hostname.2"));
